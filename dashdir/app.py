8720ead82b9ac3e70cb6b71f0714fb1c03ef8551
Francis Rossmann <frossmann@eoas.ubc.ca>
2021-10-08 13:47:52 -0700

    make a dumb graph

---
+++ dashdir/app.py
@@ -0,0 +1,24 @@
+# file app.py
+import dash
+from dash import dcc
+from dash import html
+import plotly.graph_objects as go
+
+
+es = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+app = dash.Dash(__name__, external_stylesheets=es)
+
+xs = list(range(30))
+ys = [10000 * 1.07 ** i for i in xs]
+
+fig = go.Figure(data=go.Scatter(x=xs, y=ys))
+fig.update_layout(xaxis_title="x", yaxis_title="y")
+
+app.layout = html.Div(
+    children=[html.H1(children="future daisyworld"), dcc.Graph(figure=fig)]
+)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
